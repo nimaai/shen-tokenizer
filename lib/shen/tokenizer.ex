329fc333cf1fe520effe1db95b115ae697728815
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-01-26 23:14:56 +0100

    buffer wip

--- lib/shen/tokenizer.ex
+++ lib/shen/tokenizer.ex
@@ -2,10 +2,11 @@ defmodule Shen.Tokenizer do
   # @symbol_chars ~r/[-=*\/+_?$!\@~><&%'#`;:{}a-zA-Z0-9.]/
   # require IEx
 
-  def next(io_device) do
+  def next(io_device, buffer) do
+    Process.register(buffer, :buffer)
     drain_whitespace(io_device)
-    c = readc(io_device)
-    unless c == :eof do
+    c = getc(io_device)
+    unless eof?(c) do
       case c do
         "(" -> "["
         ")" -> "]"
@@ -13,28 +14,49 @@ defmodule Shen.Tokenizer do
         _  -> c
       end
     end
+    Process.unregister(:buffer)
   end
 
-  defp readc(io_device) do
-    IO.read(io_device, 1)
+  defp pop(buffer) do
+    {List.first(buffer), Enum.drop(buffer, 1)}
+  end
+
+  defp empty?(buffer) do
+    Enum.empty?(buffer)
+  end
+
+  defp eof?(c) do
+    Agent.get(:buffer, &empty?/1) || c == :eof
+  end
+
+  defp getc(io_device) do
+    if c = Agent.get_and_update(:buffer, &pop/1)
+      c
+    else
+      IO.read(io_device, 1)
+    end
+  end
+
+  defp ungetc(c) do
+    Agent.update(:buffer, fn buffer -> [c | buffer] end)
   end
 
   defp consume_string(io_device, list_of_chars) do
-    c = readc(io_device)
+    c = getc(io_device)
+    if eof?(c) do: raise "unterminated string"
     case c do
-      :eof -> raise "unterminated string"
       "\"" -> Enum.join(list_of_chars)
       _ -> consume_string(io_device, list_of_chars ++ [c])
     end
   end
 
   defp drain_whitespace(io_device) do
-    c = readc(io_device)
-    unless c == :eof do
+    c = getc(io_device)
+    unless eof?(c) do
       if Regex.match?(~r/\s/, c) do
         drain_whitespace(io_device)
       else
-        next(io_device)
+        ungetc(c)
       end
     end
   end
