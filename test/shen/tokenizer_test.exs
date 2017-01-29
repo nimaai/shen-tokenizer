8ae9e6facf54e789866559830dcdfd22a74d51a1
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-01-30 00:16:55 +0100

    refactor tokenizer test

--- test/shen/tokenizer_test.exs
+++ test/shen/tokenizer_test.exs
@@ -1,24 +1,38 @@
 defmodule Shen.TokenizerTest do
   use ExUnit.Case
+  use ExUnit.Callbacks
   alias Shen.Tokenizer
 
-  test "drain whitespace" do
-    {:ok, io_string} = StringIO.open("  \n\r (\"foo\"   \"bar\"\t) \n")
+  setup do
     {:ok, buffer} = Agent.start_link(fn -> [] end)
-    assert Tokenizer.next(io_string, buffer) == "["
-    assert Tokenizer.next(io_string, buffer) == "foo"
-    assert Tokenizer.next(io_string, buffer) == "bar"
-    assert Tokenizer.next(io_string, buffer) == "]"
-    Agent.stop(buffer)
+    [buffer: buffer]
   end
 
-  test "unterminated string" do
-    {:ok, io_string} = StringIO.open("\"foo\" \"bar  baz")
-    {:ok, buffer} = Agent.start_link(fn -> [] end)
-    assert Tokenizer.next(io_string, buffer) == "foo"
-    assert_raise RuntimeError, "unterminated string", fn ->
-      Tokenizer.next(io_string, buffer)
+  describe "whitespace" do
+    test "is ignored between tokens", context do
+      io_string = open_io_string("  \n\r (\"foo\"   \"bar\"\t) \n")
+      assert Tokenizer.next(io_string, context[:buffer]) == "["
+      assert Tokenizer.next(io_string, context[:buffer]) == "foo"
+      assert Tokenizer.next(io_string, context[:buffer]) == "bar"
+      assert Tokenizer.next(io_string, context[:buffer]) == "]"
     end
-    Agent.stop(buffer)
+
+    test "is kept intact inside of strings", context do
+      io_string = open_io_string("     \"one two\"   ")
+      assert Tokenizer.next(io_string, context[:buffer]) == "one two"
+    end
+  end
+
+  test "unterminated string", context do
+    io_string = open_io_string("\"foo\" \"bar  baz")
+    assert Tokenizer.next(io_string, context[:buffer]) == "foo"
+    assert_raise RuntimeError, "unterminated string", fn ->
+      Tokenizer.next(io_string, context[:buffer])
+    end
+  end
+
+  defp open_io_string(string) do
+    {:ok, io_string} = StringIO.open(string)
+    io_string
   end
 end
