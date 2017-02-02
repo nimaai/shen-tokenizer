aaba2262263d5ae842e219fedf8dc554b70ed587
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-02-02 23:50:27 +0100

    tokenizer: buffer & io_device agents

--- lib/shen/tokenizer.ex
+++ lib/shen/tokenizer.ex
@@ -4,17 +4,19 @@ defmodule Shen.Tokenizer do
 
   def next(io_device, buffer) do
     Process.register(buffer, :buffer)
-    drain_whitespace(io_device)
-    c = getc(io_device)
+    Agent.start_link(fn -> io_device end, name: :io_device)
+    drain_whitespace()
+    c = getc()
     token = unless eof?(c) do
       case c do
         "(" -> "["
         ")" -> "]"
-        "\"" -> consume_string(io_device, [])
+        "\"" -> consume_string()
         _  -> c
       end
     end
     Process.unregister(:buffer)
+    Agent.stop(:io_device)
     token
   end
 
@@ -30,11 +32,11 @@ defmodule Shen.Tokenizer do
     Agent.get(:buffer, &empty?/1) && c == :eof
   end
 
-  defp getc(io_device) do
+  defp getc() do
     if c = Agent.get_and_update(:buffer, &pop/1) do
       c
     else
-      IO.read(io_device, 1)
+      Agent.get(:io_device, fn io_device -> IO.read(io_device, 1) end)
     end
   end
 
@@ -42,20 +44,20 @@ defmodule Shen.Tokenizer do
     Agent.update(:buffer, fn buffer -> [c | buffer] end)
   end
 
-  defp consume_string(io_device, list_of_chars) do
-    c = getc(io_device)
+  defp consume_string(list_of_chars \\ []) do
+    c = getc()
     if eof?(c), do: raise "unterminated string"
     case c do
       "\"" -> Enum.join(list_of_chars)
-      _ -> consume_string(io_device, list_of_chars ++ [c])
+      _ -> consume_string(list_of_chars ++ [c])
     end
   end
 
-  defp drain_whitespace(io_device) do
-    c = getc(io_device)
+  defp drain_whitespace() do
+    c = getc()
     unless eof?(c) do
       if Regex.match?(~r/\s/, c) do
-        drain_whitespace(io_device)
+        drain_whitespace()
       else
         ungetc(c)
       end
